fbd28da8f75fb06e11607f4dff35c07566566fe6
Travis <travis@travis-ci.org>
2018-10-29 22:58:54 +0000

    Travis build: 279

---
+++ api_project.js
@@ -0,0 +1,14 @@
+define({
+  "description": "Documentation",
+  "name": "geli-api",
+  "version": "0.8.0",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-10-29T22:58:53.358Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
