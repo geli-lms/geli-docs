28c9f179715ee0a0b83ad723628b01a244919f17
Travis <travis@travis-ci.org>
2018-10-30 15:27:07 +0000

    Travis build: 331

--- api_project.js
+++ api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-10-30T15:08:36.258Z",
+    "time": "2018-10-30T15:27:04.425Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
